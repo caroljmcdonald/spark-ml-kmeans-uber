9cfb7c30a6bc050815cbd3a44b0473991052b38a
Carol McDonald <caroljmcdonald@gmail.com>
2016-11-18 12:31:11 -0500

    initial commit

---
+++ readme.txt
@@ -0,0 +1,31 @@
+Install and fire up the Sandbox using the instructions here: http://maprdocs.mapr.com/home/SandboxHadoop/c_sandbox_overview.html. 
+
+Use an SSH client such as Putty (Windows) or Terminal (Mac) to login. See below for an example:
+use userid: user01 and password: mapr.
+
+For VMWare use:  $ ssh user01@ipaddress 
+
+For Virtualbox use:  $ ssh user01@127.0.0.1 -p 2222 
+
+
+You can build this project with Maven using IDEs like Eclipse or NetBeans, and then copy the JAR file to your MapR Sandbox, or you can install Maven on your sandbox and build from the Linux command line.
+
+You can use scp to copy your JAR file to the MapR Sandbox:
+
+use userid: user01 and password: mapr.
+For VMWare use:  $ scp  nameoffile.jar  user01@ipaddress:/user/user01/. 
+
+For Virtualbox use:  $ scp -P 2222 nameoffile.jar  user01@127.0.0.1:/user/user01/.  
+
+Copy the data file using scp to here:
+
+/user/user01/data/uber.csv
+
+
+To Run the Spark Consumer Producer (in separate consoles if you want to run at the same time) run the spark consumer with the topic to read from and write to:
+
+spark-submit --class com.sparkml.uber.ClusterUber --master local[2] --packages com.databricks:spark-csv_2.10:1.5.0  spark-kmeans-1.0.jar  
+
+    
+ - For Yarn you should change --master parameter to yarn-client - "--master yarn-client"
+
---
+++ src/main/scala/com/sparkml/uber/SqlUber.scala
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sparkml.uber
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.util.IntParam
+import org.apache.spark.sql.SQLContext
+
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+
+import org.apache.spark.mllib.stat.Statistics
+
+object SqlUber {
+
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf().setAppName("SparkDFebay")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+
+    import sqlContext.implicits._
+    import sqlContext._
+
+
+
+    val df = sqlContext.read.json("uberclusterstest").cache()
+
+    df.printSchema
+
+    // COMMAND ----------
+
+    df.show
+
+    df.registerTempTable("uber")
+    //
+    df.select(month($"dt").alias("month"), dayofmonth($"dt").alias("day"), $"prediction").groupBy("month", "day", "prediction").agg(count("prediction").alias("count")).orderBy(desc("count")).show
+
+    // COMMAND ----------
+
+    df.select(month($"dt").alias("month"), dayofmonth($"dt").alias("day"), hour($"dt").alias("hour"), $"prediction").groupBy("month", "day", "hour", "prediction").agg(count("prediction").alias("count")).orderBy(desc("count"), desc("prediction")).show
+
+    df.select(month($"dt").alias("month"), dayofmonth($"dt").alias("day"), hour($"dt").alias("hour"), $"prediction").groupBy("month", "day", "prediction").agg(count("prediction").alias("count")).orderBy(asc("day")).show
+    // COMMAND ----------
+    df.select(month($"dt").alias("month"), dayofmonth($"dt").alias("day"), hour($"dt").alias("hour"), $"prediction").groupBy("month", "day", "hour", "prediction").agg(count("prediction").alias("count")).orderBy("day", "hour", "prediction").show
+
+    df.select(month($"dt").alias("month"), dayofmonth($"dt").alias("day"), hour($"dt").alias("hour"), $"prediction").groupBy("month", "day", "hour", "prediction").agg(count("prediction").alias("count")).orderBy("day", "hour", "prediction").show
+
+
+    // List and also count the number of times the closing price for SPY went up or down by more than 4 
+    var res = sqlContext.sql("SELECT spy.dt, spy.openprice, spy.closeprice, abs(spy.closeprice - spy.openprice) as spydif FROM spy WHERE abs(spy.closeprice - spy.openprice) > 4 ")
+    res.show
+    res.count
+
+   
+  }
+}
